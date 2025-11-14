b1e027e0a95285b4da6ef7e021a3a9d236846fe6
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-11-13 23:56:39 -0300

    fix: update HTML language attribute to Portuguese (pt-BR)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html lang="pt-BR" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
       </head>
