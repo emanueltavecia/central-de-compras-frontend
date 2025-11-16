1822c9afa51d0098a24151afc9ae676bf067c316
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-11-16 20:27:00 -0300

    feat(layout): add TokenInitializer component to layout for authentication handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { Inter } from 'next/font/google'
 
 import {
@@ -7,6 +9,8 @@ import {
 } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import type { Metadata } from 'next'
+
+import { TokenInitializer } from '@/components/auth/token-initializer'
 
 import '@mantine/core/styles.css'
 import '@mantine/notifications/styles.css'
